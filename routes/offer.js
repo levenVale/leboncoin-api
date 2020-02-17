b2b408efe683bf507896cd064a52e65bc7fd728d
Sebastien KEMPF <kemfou@thegrid.lan>
2020-02-17 16:09:02 +0100

    implemented Cloudinary to backend and database after debug

--- routes/offer.js
+++ routes/offer.js
@@ -10,58 +10,55 @@ const isAuthenticated = require("../middleware/isAuthenticated");
 
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({
-  cloud_name: "process.env.CLOUD_NAME",
-  api_key: "process.env.CLOUDINARY_API_KEY",
-  api_secret: "process.env.CLOUDINARY_API_SECRET"
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 // CREATE
 router.post("/offer/publish", isAuthenticated, async (req, res) => {
   try {
-    cloudinary.uploader.upload(req.file.path, (error, result) => {
+    cloudinary.uploader.upload(req.files.files.path, async (error, result) => {
       if (error) {
         return res.json({ error: `Upload Error` });
       } else {
-        return res.json(result);
+        const newPost = await new Offer({
+          title: req.fields.title,
+          description: req.fields.description,
+          price: req.fields.price,
+          pictures: result.secure_url,
+          created: new Date(),
+          creator: req.user.populate("account")
+        });
+        if (req.fields.description.length > 500) {
+          return res
+            .status(400)
+            .json({ message: "Description must be 500 characters or less" });
+        }
+        if (req.fields.title.length > 50) {
+          return res
+            .status(400)
+            .json({ error: "Title must be 50 characters or less" });
+        }
+        if (req.fields.price > 100000) {
+          return res
+            .status(400)
+            .json({ error: "Price must be 100 000 or less" });
+        } else {
+          res.json({
+            _id: newPost._id,
+            title: newPost.title,
+            price: newPost.price,
+            created: newPost.created,
+            creator: {
+              account: { username: newPost.creator.account.username },
+              _id: newPost.creator.id
+            }
+          });
+          await newPost.save();
+        }
       }
     });
-
-    console.log(res.json(result));
-
-    const newPost = await new Offer({
-      title: req.fields.title,
-      description: req.fields.description,
-      price: req.fields.price,
-      // pictures: req.files,
-      created: new Date(),
-      creator: req.user.populate("account")
-    });
-
-    if (req.fields.description.length > 500) {
-      return res
-        .status(400)
-        .json({ message: "Description must be 500 characters or less" });
-    }
-    if (req.fields.title.length > 50) {
-      return res
-        .status(400)
-        .json({ error: "Title must be 50 characters or less" });
-    }
-    if (req.fields.price > 100000) {
-      return res.status(400).json({ error: "Price must be 100 000 or less" });
-    } else {
-      res.json({
-        _id: newPost._id,
-        title: newPost.title,
-        price: newPost.price,
-        created: newPost.created,
-        creator: {
-          account: { username: newPost.creator.account.username },
-          _id: newPost.creator.id
-        }
-      });
-      await newPost.save();
-    }
   } catch (error) {
     res.json(error.message);
   }
