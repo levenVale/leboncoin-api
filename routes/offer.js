efcc8f4d488aad332e48410e22a222c5153bc505
Sebastien KEMPF <kemfou@thegrid.lan>
2020-02-17 13:34:22 +0100

    implemented Cloudinary

--- routes/offer.js
+++ routes/offer.js
@@ -10,7 +10,7 @@ const isAuthenticated = require("../middleware/isAuthenticated");
 
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({
-  cloud_name: "dzzyt6yhh",
+  cloud_name: "process.env.CLOUD_NAME",
   api_key: "process.env.CLOUDINARY_API_KEY",
   api_secret: "process.env.CLOUDINARY_API_SECRET"
 });
